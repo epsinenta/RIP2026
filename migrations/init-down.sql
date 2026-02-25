b30bcd82d4560fb51dbb0f2772b1282c90fba1cb
Vlastelin <berdnikov.nikolay05@gmail.com>
2026-02-25 14:53:01 +0300

    add down migrate

---
+++ migrations/init-down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS department_application_departments CASCADE;
+DROP TABLE IF EXISTS department_applications CASCADE;
+DROP TABLE IF EXISTS departments CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
